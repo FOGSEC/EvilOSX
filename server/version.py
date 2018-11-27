872792e7cdc6ebd925367fd1ec2ddfa61cbdda56
Marten4n6 <Marten4n6@protonmail.com>
2018-11-27 20:44:11 +0100

    Add versioning

---
+++ server/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Keeps track of the current server version.
+
+For more information on SemVer, please visit https://semver.org/.
+"""
+__author__ = "Marten4n6"
+__license__ = "GPLv3"
+
+VERSION = "1.0.0"
